6a75238a47aa0593490b105f073d02c7d324850b
MASTER718 <96640794+MASTER718@users.noreply.github.com>
2022-02-26 19:31:07 +0800

    Update count.sol

--- 第一周/count.sol
+++ 第一周/count.sol
@@ -1,1 +1,26 @@
-pragma 
+pragma solidity ^0.8.0;
+
+contract Counter{
+
+   uint counter;
+
+   constructor() public {
+
+    counter = 0;
+
+   }
+   function count() public {
+
+  counter = counter +1;
+
+
+   }
+   function get() public view returns (uint) {
+   return counter ;
+  
+   }
+
+
+
+
+}
