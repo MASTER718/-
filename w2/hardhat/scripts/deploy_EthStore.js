bd2b4d9c331b516394f7a657dcf6e79d481c81d1
sue <2391531207@qq.com>
2022-03-02 19:04:00 +0800

    环境更新

---
+++ w2/hardhat/scripts/deploy_EthStore.js
@@ -0,0 +1,25 @@
+
+const {ethers}      = require("hardhat");
+const {writeAddr} = require('./artifact_log.js');
+
+
+
+async function main(){
+
+
+       const [developer] = await  ethers.getSigners();
+       console.log("developer address:"+deployer.address)
+       
+
+
+
+
+
+
+
+
+
+
+
+
+}
